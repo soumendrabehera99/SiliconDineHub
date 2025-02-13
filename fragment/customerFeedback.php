3b1a2a66deb86a168e65b965286e242126d272b9
Soumendra Kumar <soumendrabehera99@gmail.com>
2025-02-13 11:01:41 +0530

    carousel content updated

--- fragment/customerFeedback.php
+++ fragment/customerFeedback.php
@@ -9,76 +9,64 @@
                     <div class="row g-2">
                         <div class="col-md-6 ">
                             <div class="text-white p-3 bg-dark rounded-4">
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dolor quibusdam, vel
-                                    iure officiis ducimus fuga, veniam obcaecati provident voluptates, quidem sunt alias
-                                    velit repellat mollitia laborum! Optio, pariatur dolorem.
+                                <p class="text-truncate-2">
+                                The pre-booking system is a lifesaver! No more long queues, and I get my food on time. The cafeteria is well-organized, and the food is always fresh. Love it!
                                 </p>
-                                <h4>Soumendra Kumar</h4>
-                                <p>Lorem, ipsum.</p>
+                                <h4>Rohan Verma</h4>
+                                <p>Computer Science Student</p>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="text-white p-3 bg-dark rounded-4">
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dolor quibusdam, vel
-                                    iure officiis ducimus fuga, veniam obcaecati provident voluptates, quidem sunt alias
-                                    velit repellat mollitia laborum! Optio, pariatur dolorem.
+                                <p class="text-truncate-2">
+                                Great food at pocket-friendly prices! The variety of meals is amazing, and the taste is just like homemade. I appreciate how hygienic everything is!
                                 </p>
-                                <h4>Soumendra Kumar</h4>
-                                <p>Lorem, ipsum.</p>
+                                <h4>Ananya Sharma</h4>
+                                <p>MBA Student</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="carousel-item active">
+                <div class="carousel-item">
                     <div class="row g-2">
                         <div class="col-md-6 ">
                             <div class="text-white p-3 bg-dark rounded-4">
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dolor quibusdam, vel
-                                    iure officiis ducimus fuga, veniam obcaecati provident voluptates, quidem sunt alias
-                                    velit repellat mollitia laborum! Optio, pariatur dolorem.
+                                <p class="text-truncate-2">
+                                    I love how fast the service is! The four counters make food collection super easy, even during rush hours. No stress, just good food!
                                 </p>
-                                <h4>Soumendra Kumar</h4>
-                                <p>Lorem, ipsum.</p>
+                                <h4>Rahul Mehta</h4>
+                                <p>Electrical Engineering Student.</p>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="text-white p-3 bg-dark rounded-4">
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dolor quibusdam, vel
-                                    iure officiis ducimus fuga, veniam obcaecati provident voluptates, quidem sunt alias
-                                    velit repellat mollitia laborum! Optio, pariatur dolorem.
+                                <p class="text-truncate-2">
+                                The cafeteria system helps me track my food expenses effortlessly. At the end of the month, I can see my spending and plan accordingly. Smart and efficient!
                                 </p>
-                                <h4>Soumendra Kumar</h4>
-                                <p>Lorem, ipsum.</p>
+                                <h4>Priya Das</h4>
+                                <p>Biotechnology Student</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="carousel-item active">
+                <div class="carousel-item">
                     <div class="row g-2">
                         <div class="col-md-6 ">
                             <div class="text-white p-3 bg-dark rounded-4">
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dolor quibusdam, vel
-                                    iure officiis ducimus fuga, veniam obcaecati provident voluptates, quidem sunt alias
-                                    velit repellat mollitia laborum! Optio, pariatur dolorem.
+                                <p class="text-truncate-2">
+                                Finally, a college cafeteria that offers healthy yet tasty meals! I always find fresh salads, juices, and balanced meals. Highly recommend it!
                                 </p>
-                                <h4>Soumendra Kumar</h4>
-                                <p>Lorem, ipsum.</p>
+                                <h4>Siddharth Kapoor</h4>
+                                <p>MCA Student</p>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="text-white p-3 bg-dark rounded-4">
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dolor quibusdam, vel
-                                    iure officiis ducimus fuga, veniam obcaecati provident voluptates, quidem sunt alias
-                                    velit repellat mollitia laborum! Optio, pariatur dolorem.
+                                <p class="text-truncate-2">
+                                    This cafeteria has completely changed my college experience! The food is amazing, the service is super fast, and the pre-booking option is a genius move. I no longer have to skip meals due to long queues!
                                 </p>
-                                <h4>Soumendra Kumar</h4>
-                                <p>Lorem, ipsum.</p>
+                                <h4>Sneha Rao</h4>
+                                <p>Electronics & Communication Student</p>
                             </div>
                         </div>
                     </div>
--- index.php
+++ index.php
@@ -14,9 +14,8 @@ include_once "./fragment/navbar.php";
                                 <div class="row">
                                     <div class="col-md-7 col-lg-6 p-3 text-justify">
                                         <div class="details">
-                                            <h1 class="text-white">Slider Text Zero.</h1>
-                                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illum quisquam
-                                                facilis natus! Dolores veniam illo esse consequatur inventore at.</p>
+                                            <h1 class="text-white">Good Food, Good Mood!</h1>
+                                            <p>Great food makes a great day! Our cafeteria serves fresh, hygienic, and budget-friendly meals to keep you energized. Whether it’s a quick bite or a full meal, we’ve got you covered! 🍽️✨</p>
                                         </div>
                                     </div>
                                 </div>
@@ -27,9 +26,8 @@ include_once "./fragment/navbar.php";
                                 <div class="row">
                                     <div class="col-md-7 col-lg-6 p-3 text-justify">
                                         <div class="details">
-                                            <h1 class="text-white">Slider Text One.</h1>
-                                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illum quisquam
-                                                facilis natus! Dolores veniam illo esse consequatur inventore at.</p>
+                                            <h1 class="text-white">No More Waiting in Long Queues!</h1>
+                                            <p>Pre-book your meal and pick it up hassle-free! Save time and enjoy your break to the fullest. ⏳🍕</p>
                                         </div>
                                     </div>
                                 </div>
@@ -40,9 +38,8 @@ include_once "./fragment/navbar.php";
                                 <div class="row">
                                     <div class="col-md-7 col-lg-6 p-3 text-justify">
                                         <div class="details">
-                                            <h1 class="text-white">Slider Text Two</h1>
-                                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illum quisquam
-                                                facilis natus! Dolores veniam illo esse consequatur inventore at.</p>
+                                            <h1 class="text-white">Multiple Counters for Fast Service</h1>
+                                            <p>Collect your meal from your assigned counter and enjoy a smooth, stress-free dining experience. 🏃‍♂️🥗</p>
                                         </div>
                                     </div>
                                 </div>
@@ -53,9 +50,8 @@ include_once "./fragment/navbar.php";
                                 <div class="row">
                                     <div class="col-md-7 col-lg-6 p-3 text-justify">
                                         <div class="details">
-                                            <h1 class="text-white">Slider Text Three.</h1>
-                                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illum quisquam
-                                                facilis natus! Dolores veniam illo esse consequatur inventore at.</p>
+                                            <h1 class="text-white">Eat Smart, Spend Smarter!</h1>
+                                            <p>Track your food expenses and manage your budget with ease. Your monthly meal history, all in one place! 📊💰</p>
                                         </div>
                                     </div>
                                 </div>
