1964049a43f0f55bc3f8d9168bbbb5eee33486e7
Anil <anilsahu9786@gmail.com>
2025-02-13 22:45:19 +0530

    useful links col. added in footer

--- fragment/footer.php
+++ fragment/footer.php
@@ -2,7 +2,7 @@
     <footer class="footer_section text-center text-white bg-dark mt-4">
       <div class="container">
         <div class="row g-5">
-          <div class="col-md-4 footer-col">
+          <div class="col-md-3">
             <div class="footer_contact">
               <h4>Contact Us</h4>
               <div class="contact_link_box">
@@ -32,7 +32,7 @@
               </div>
             </div>
           </div>
-          <div class="col-md-4 footer-col">
+          <div class="col-md-3">
             <div class="footer_detail">
               <a href="" class="footer-logo"> Silicon DineHub </a>
               <p>
@@ -56,61 +56,58 @@
               </div>
             </div>
           </div>
-          <div class="col-md-4 footer-col">
+          <div class="col-md-3">
               <h4>Opening Hours</h4>
-              <div class="row">
-                  <div class="col-12 d-flex justify-content-between flex-wrap">
-                      <div class="col-6 col-sm-5 text-center">
+              <div class="row g-0 m-2 text-start">
+                      <div class="col-6 col-md-5">
                           <p>Mon-Sat</p>
                           <p>Sunday</p>
                           <p>Holiday</p>
                       </div>
-                      <div class="col-6 col-sm-6 text-start">
+                      <div class="col-6 col-md-7">
                           <p>10.00 AM - 10.00 PM</p>
                           <p>02.30 PM - 07.00 PM</p>
                           <p>02.30 PM - 07.00 PM</p>
                       </div>
-                  </div>
+              </div>
+          </div>
+          <div class="col-md-3">
+              <h5>Useful Links</h5>
+              <div class="row">
+                <div class="col-md-12">     
+                  <ul class="list-unstyled mt-4 ms-5">
+                    <li class="text-start">
+                      <a href="#contactus" class="text-decoration-none text-white">Contact Us</a>
+                    </li>
+                    <li class="text-start mt-2">
+                      <a href="./admin/adminLogin.php" class="text-decoration-none text-white">Admin</a>
+                    </li>
+                    <li class="text-start mt-2">
+                      <a href="./privacyAndPolicy.php" class="text-decoration-none text-white">Privacy Policy</a>
+                    </li>
+                  </ul>
+                </div>
               </div>
           </div>
         </div>
-        
-        <div class="row mt-1">
-          <div class="col-md-12">
-            <h5 class="my-3">Useful Links</h5>
-            <ul class="list-inline">
-              <li class="list-inline-item mx-3">
-                <a href="#contactus" class="text-decoration-none text-white">Contact Us</a>
-              </li>
-              <li class="list-inline-item mx-3">
-                <a href="./admin/adminLogin.php" class="text-decoration-none text-white">Admin</a>
-              </li>
-              <li class="list-inline-item mx-3">
-                <a href="./privacyAndPolicy.php" class="text-decoration-none text-white">Privacy Policy</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div class="footer-info text-center">
-          <p>
-            &copy; <span id="displayYear"></span> All Rights Reserved By
-            <a href="">Silicon DineHub</a>
-            <script>
-              document.getElementById("displayYear").innerText = new Date().getFullYear();
-            </script>
-          </p>
-        </div>
+      </div>
+      <div class="footer-info text-center">
+        <p>
+          &copy; <span id="displayYear"></span> All Rights Reserved By
+          <a href="">Silicon DineHub</a>
+          <script>
+            document.getElementById("displayYear").innerText = new Date().getFullYear();
+          </script>
+        </p>
       </div>
     </footer>
     <!-- footer section -->
 
     <!-- Bootstrap JS -->
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> -->
     <script src="./assets/bootstrap/bootstrap.bundle.min.js"></script>
     
     <!-- JQuery -->
-    <!-- <script src="../assets/jquery/jquery-3.7.1.min.js"></script> -->
+    <script src="../assets/jquery/jquery-3.7.1.min.js"></script>
 
     <!-- custom js -->
     <script src="./assets/jquery/jquery-3.7.1.min.js"></script>
